845b8e03bef7c5c14fa290663b7bd07921df205d
yoshihiro <yossy8931@gmail.com>
2021-03-30 11:02:50 +0900

    first-commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="footer">
+      <footer class="top-footer">
+        <div class="footer-bar">
+          <div class="flex">
+            <h1 class="header-logo">estra inc.</h1>
+            <nav class="header-nav">
+              <ul>
+                <li><a href="#">home</a></li>
+                <li><a href="#">about</a></li>
+                <li><a href="#">service</a></li>
+                <li><a href="#">works</a></li>
+                <li><a href="#">price</a></li>
+                <li><a href="#">contact</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <small>Copyright 2019 estra inc. All Rights Reserved.</small>
+      </footer>
+  </div>
+</template>
+
+
+<style>
+.footer-bar {
+  height: 70px;
+  line-height: 70px;
+}
+
+.top-footer {
+  background: #000;
+  text-align: center;
+}
+
+.top-footer small {
+  text-align: center;
+  color: #fff;
+}
+</style>
